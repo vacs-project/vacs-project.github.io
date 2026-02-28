68827c62063730a045538020ba9e42cfdf1905d1
Nick Müller <nick.mueller@mailbox.org>
2026-02-28 21:06:34 +0100

    define custom color paletts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
     title: "vacs docs",
-    tagline: "Dinosaurs are cool",
+    tagline: "VATSIM ATC Communication System",
     favicon: "img/favicon.ico",
 
     // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
