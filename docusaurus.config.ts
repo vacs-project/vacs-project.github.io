6c472c414794c100d295f78a78ef200e4c3a86ad
Nick Müller <nick.mueller@mailbox.org>
2026-03-07 17:11:50 +0100

    add remote API docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -117,6 +117,7 @@ const config: Config = {
         prism: {
             theme: prismThemes.github,
             darkTheme: prismThemes.dracula,
+            additionalLanguages: ["toml"],
         },
     } satisfies Preset.ThemeConfig,
 };
